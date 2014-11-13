65bdf625f8861da06a47aa78a5cae0b5a066f1e1
Kemal Eren <kemal@kemaleren.com>
2014-11-12 16:58:59 -0800

    initial script

---
+++ backtranslate.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+from Bio.Alphabet import Gapped
+
+
+def back_translate_gapped(protein, dna):
+    """Insert gaps from `protein` into ungapped back-translated `dna`.
+
+    >>> back_translate_gapped("MK-V", "ATGAAAGTG")
+    "ATGAAA---GTG"
+
+    """
+    gap_char = protein.seq.alphabet.gap_char
+    gap_codon = dna.seq.alphabet.gap_char * 3
+    dna_str = str(dna.seq.ungap())
+    codons = []
+    for a in protein.seq:
+        if a == gap_char:
+            codons.append(gap_codon)
+        else:
+            codons.append(dna_str[:3])
+            dna_str = dna_str[3:]
+    result = dna[:]
+    result.seq = Seq("".join(codons), alphabet=dna.seq.alphabet)
+    return result
+
+
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv[1:]
+    if len(args) != 2:
+        sys.stderr.write("usage: backtranslate.py <gapped protein fasta> <ungapped dna fasta>\n")
+        sys.stderr.write("       prints result to STDOUT\n")
+        sys.exit()
+
+    translated_filename = args[0]
+    gapless_filename = args[1]
+
+    translated_handle = open(translated_filename, "rU")
+    gapless_handle = open(gapless_filename, "rU")
+    translated_records = SeqIO.parse(translated_handle, "fasta", alphabet=Gapped(IUPAC.protein))
+    gapless_records = SeqIO.parse(gapless_handle, "fasta", alphabet=Gapped(IUPAC.unambiguous_dna))
+    result_iter = (back_translate_gapped(a, b) for a, b in zip(translated_records, gapless_records))
+    SeqIO.write(result_iter, sys.stdout, "fasta")
+    translated_handle.close()
+    gapless_handle.close()
