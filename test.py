9ae338edcdc9c2367ffe9d93b855d17e9dbbee12
Kemal Eren <kemal@kemaleren.com>
2014-11-12 17:16:53 -0800

    wrote simple test

---
+++ test.py
@@ -0,0 +1,24 @@
+import unittest
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import IUPAC
+from Bio.Alphabet import Gapped
+
+from backtranslate import back_translate_gapped
+
+
+class TestBacktranslate(unittest.TestCase):
+
+    def compare_seqrecords(self, a, b):
+        # TODO: this is probably not the best way to compare seqrecords
+        self.assertEqual(str(a), str(b))
+        self.assertEqual(str(a.seq.alphabet), str(b.seq.alphabet))
+
+    def test_simple(self):
+        protein = SeqRecord(Seq("MK-V", alphabet=Gapped(IUPAC.protein)))
+        dna = SeqRecord(Seq("ATGAAAGTG", alphabet=IUPAC.unambiguous_dna))
+        expected = SeqRecord(Seq("ATGAAA---GTG", alphabet=Gapped(IUPAC.unambiguous_dna)))
+        result = back_translate_gapped(protein, dna)
+        self.compare_seqrecords(expected, result)
