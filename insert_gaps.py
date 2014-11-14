c0bc1770e422ae4087c1fc1a9c28284c4c39b551
Kemal Eren <kemal@kemaleren.com>
2014-11-13 16:10:45 -0800

    started scripts for inserting and removing gaps

---
+++ insert_gaps.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Inserts gaps into output of align_to_clean.py to make MSA with clean MSA.
+
+Usage:
+  insert_gaps.py <clean_msa_fasta> <dirty_aligned_fasta>
+  insert_gaps.py -h | --help
+
+Options:
+  -h --help      Show this screen.
+
+"""
---
+++ remove_gaps.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+Removes gaps from MSA fasta to prepare clean reads for
+align_to_clean.py.
+
+Usage:
+  remove_gaps.py <clean_msa_fasta>
+  remove_gaps.py -h | --help
+
+Options:
+  -h --help      Show this screen.
+
+"""
