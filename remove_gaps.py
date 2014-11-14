fe05d435c07992540f48305fafdb5cc427770c49
Kemal Eren <kemal@kemaleren.com>
2014-11-13 16:58:23 -0800

    wrote remove_gaps.py

--- remove_gaps.py
+++ remove_gaps.py
@@ -11,3 +11,23 @@ Options:
   -h --help      Show this screen.
 
 """
+import sys
+
+from docopt import docopt
+from Bio import SeqIO
+from Bio.Alphabet import IUPAC
+from Bio.Alphabet import Gapped
+
+
+def ungap(record):
+    record.seq = record.seq.ungap()
+    return record
+
+
+if __name__ == "__main__":
+    args = docopt(__doc__)
+    infile = args["<clean_msa_fasta>"]
+    handle = open(infile, "rU")
+    records = SeqIO.parse(handle, "fasta", alphabet=Gapped(IUPAC.unambiguous_dna))
+    results = (ungap(r) for r in records)
+    SeqIO.write(results, sys.stdout, "fasta")
