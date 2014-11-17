7aee9e9205089dbe5c67e308aecad96b0426925e
Kemal Eren <kemal@kemaleren.com>
2014-11-17 14:35:55 -0800

    fixed doctests and assertion

--- util.py
+++ util.py
@@ -1,17 +1,17 @@
 def insert_gaps(source, target, src_gap, target_gap, skip):
     """Inserts `gap` into target string at same positions as in `source`.
 
-    > insert_gaps("a-bc", "def", "-", "-", 1)
-    "d-ef"
+    >>> insert_gaps("a-bc", "def", "-", "-", 1)
+    'd-ef'
 
-    > insert_gaps("a-bc", "def", "-", "--", 1)
-    "d--ef"
+    >>> insert_gaps("a-bc", "def", "-", "--", 1)
+    'd--ef'
 
-    > insert_gaps("a-bc", "dddeeefff", "-", "---", 3)
-    "ddd---eeefff"
+    >>> insert_gaps("a-bc", "dddeeefff", "-", "---", 3)
+    'ddd---eeefff'
 
     """
-    assert len(c for c in source if s != src_gap) == len(target)
+    assert len(list(c for c in source if c != src_gap)) * skip == len(target)
     result = []
     for c in source:
         if c == src_gap:
