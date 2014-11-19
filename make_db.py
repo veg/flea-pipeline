9a03bd15e0867bc183f6151169e1c19efe6efbef
Kemal Eren <kemal@kemaleren.com>
2014-11-19 13:46:58 -0800

    make_db does not need to remove gaps

--- make_db.py
+++ make_db.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 """
-Removes gaps from reads and makes a usearch database.
+Makes a usearch database.
 
 Usage:
-  make_db.py <infile> <outfile> <dbfile>
+  make_db.py <infile> <dbfile>
   make_db.py -h | --help
 
 Options:
@@ -21,20 +21,6 @@ from Bio.Alphabet import IUPAC
 from Bio.Alphabet import Gapped
 
 
-def ungap(record):
-    record.seq = record.seq.ungap()
-    return record
-
-
-def remove_gaps(infile, outfile):
-    records = SeqIO.parse(infile, "fasta", alphabet=Gapped(IUPAC.unambiguous_dna))
-    results = (ungap(r) for r in records)
-    # need to have length multiple of 3, for codon alignment downstream
-    # TODO: warn if any have wrong length.
-    results = (r for r in results if (len(r) % 3) == 0)
-    SeqIO.write(results, outfile, "fasta")
-
-
 def make_db(infile, dbfile):
     """Make usearch database of reads in `infile`."""
     kwargs = dict(infile=infile, dbfile=dbfile)
@@ -45,7 +31,5 @@ def make_db(infile, dbfile):
 if __name__ == "__main__":
     args = docopt(__doc__)
     infile = args["<infile>"]
-    outfile = args["<outfile>"]
     dbfile = args["<dbfile>"]
-    remove_gaps(infile, outfile)
-    make_db(outfile, dbfile)
+    make_db(infile, dbfile)
