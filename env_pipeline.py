df800b96d279a36271ed79a7661b03cd0c7ea4e3
Kemal Eren <kemal@kemaleren.com>
2015-01-22 23:11:21 -0800

    expanded config file

--- env_pipeline.py
+++ env_pipeline.py
@@ -74,24 +74,9 @@ parser.add_argument('--config', type=str,
                     help='Configuration file.')
 parser.add_argument('-j', '--jobs', type=int,
                     help='Number of simultaneous local jobs.')
-parser.add_argument('--id', default='0.99', type=str,
-                    help='Percent identity for clustering.')
-parser.add_argument('--min-cluster', default=5, type=int,
-                    help='Minimum cluster size; smaller clusters are discarded.')
-parser.add_argument('--max-cluster', default=30, type=int,
-                    help='Maximum cluster size; larger clusters are subsampled.')
-parser.add_argument('--align-full', action='store_true',
-                    help='Generate full alignment of all sequences.')
-parser.add_argument('--trees', action='store_true',
-                    help='Generate trees.')
 
 
 options = parser.parse_args()
-if options.min_cluster < 1:
-    raise Exception('Bad minimum cluster size: {}'. format(options.min_cluster))
-if options.max_cluster < 1:
-    raise Exception('Bad maximum cluster size: {}'. format(options.max_cluster))
-
 
 # standard python logger which can be synchronised across concurrent
 # Ruffus tasks
@@ -117,16 +102,13 @@ hyphy_data_dir = os.path.join(data_dir, "hyphy_data")
 hyphy_input_dir = os.path.join(hyphy_data_dir, "input")
 hyphy_results_dir = os.path.join(hyphy_data_dir, "results")
 
-
 config = ConfigParser(interpolation=ExtendedInterpolation())
-try:
-    configfile = parser.config
-except AttributeError:
-    configfile = os.path.join(script_dir, 'env_pipeline.config')
-config.read(configfile)
 
-contaminant_db = config['Paths']['contaminants_db']
-reference_db = config['Paths']['reference_db']
+if options.config is None:
+    configfile = os.path.join(script_dir, 'env_pipeline.config')
+else:
+    configfile = options.config
+config.read(configfile)
 
 
 def hyphy_script(name):
@@ -177,18 +159,21 @@ def filter_fastq(infile, outfile):
 def filter_contaminants(infile, outfiles):
     uncontam, contam = outfiles
     call('usearch -usearch_global {infile} -db {db}'
-         ' -id 0.98 -notmatched {uncontam} -matched {contam}'
-         ' -strand both'.format(infile=infile, db=contaminant_db,
+         ' -id {id} -notmatched {uncontam} -matched {contam}'
+         ' -strand both'.format(infile=infile, db=config['Paths']['contaminants_db'],
+                                id=config['Parameters']['contaminant_identity'],
                                 uncontam=uncontam, contam=contam))
+
 
 @transform(filter_contaminants, suffix('.uncontaminated.fasta'),
            '.uncontaminated.matches.fasta')
 def filter_uncontaminated(infiles, outfile):
     uncontam, _ = infiles
-    call('usearch -usearch_global {uncontam} -db {db} -id 0.8'
-         ' -fastapairs {outfile} -strand both'.format(uncontam=uncontam,
-                                                     db=reference_db,
-                                                      outfile=outfile))
+    call('usearch -usearch_global {uncontam} -db {db} -id {id}'
+         ' -fastapairs {outfile} -strand both'.format(
+            uncontam=uncontam, db=config['Paths']['reference_db'],
+            id=config['Parameters']['reference_identity'],
+            outfile=outfile))
 
 
 @transform(filter_uncontaminated, suffix('.fasta'), '.shift-corrected.fasta')
@@ -213,8 +198,9 @@ def cluster(infile, outfiles, *args):
         os.unlink(f)
     outdir = '{}.clusters'.format(infile[:-len('.fasta')])
     outpattern = os.path.join(outdir, 'cluster_')
-    call('usearch -cluster_smallmem {infile} -id {identity}'
-         ' -clusters {outpattern}'.format(infile=infile, identity=options.id,
+    call('usearch -cluster_smallmem {infile} -id {id}'
+         ' -clusters {outpattern}'.format(infile=infile,
+                                          id=config['Parameters']['cluster_identity'],
                                           outpattern=outpattern))
 
     # TODO: put this in own function
@@ -228,15 +214,16 @@ def cluster(infile, outfiles, *args):
 @transform(cluster, suffix('.fasta'), '.keep.fasta')
 def select_cluster(infile, outfile):
     records = list(SeqIO.parse(infile, 'fasta'))
-    if len(records) < options.min_cluster:
-        # empty keep file; needed for pipeline sanity
-        touch(outfile)
-    if len(records) > options.max_cluster:
-        records = sample(records, options.max_cluster)
+    minsize = int(config['Parameters']['min_cluster_size'])
+    maxsize = int(config['Parameters']['max_cluster_size'])
+    if len(records) < minsize:
+        touch(outfile)  # empty file; needed for pipeline sanity
+    if len(records) > maxsize:
+        records = sample(records, maxsize)
     SeqIO.write(records, outfile, 'fasta')
 
 
-@jobs_limit(config['Jobs']['cluster_jobs'])
+@jobs_limit(int(config['Jobs']['cluster_jobs']))
 @transform(select_cluster, suffix('.fasta'), '.aligned.fasta')
 @maybe
 def align_cluster(infile, outfile):
@@ -393,7 +380,7 @@ def run_fubar(infile, outfile):
     hyphy_call(hyphy_script('runFUBAR.bf'), hyphy_data_dir)
 
 
-@active_if(options.align_full)
+@active_if(config.getboolean('Tasks', 'align_full'))
 @transform(shift_correction,
            suffix('.shift-corrected.fasta'),
            add_inputs([cat_all_perfect, backtranslate_alignment, make_full_db]),
@@ -410,13 +397,14 @@ def align_full_timestep(infiles, outfile):
     align_to_refs(shift_corrected, perfect, perfect_aligned, dbfile, outfile)
 
 
-@active_if(options.align_full)
+@active_if(config.getboolean('Tasks', 'align_full'))
 @merge(align_full_timestep, 'all_timepoints.aligned.fasta')
 def merge_all_timepoints(infiles, outfile):
     cat_files(infiles, outfile)
 
 
-@active_if(options.align_full and options.trees)
+@active_if(config.getboolean('Tasks', 'align_full') and
+           config.getboolean('Tasks', 'generate_trees'))
 @transform([align_full_timestep, merge_all_timepoints],
            suffix('.fasta'), '.tre')
 def infer_trees(infile, outfile):
