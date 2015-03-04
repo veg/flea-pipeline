a931a56a80808436dab0e98901c25f507d8a18de
Kemal Eren <kemal@kemaleren.com>
2015-03-03 20:17:43 -0800

    updated backtranslate tests

--- test_backtranslate.py
+++ test_backtranslate.py
@@ -7,7 +7,6 @@ from Bio.Alphabet import IUPAC
 from Bio.Alphabet import Gapped
 
 from backtranslate import back_translate_gapped
-from backtranslate import MissingRecord
 
 
 class TestBacktranslate(unittest.TestCase):
@@ -23,10 +22,3 @@ class TestBacktranslate(unittest.TestCase):
         expected = SeqRecord(Seq("ATGAAA---GTG", alphabet=Gapped(IUPAC.unambiguous_dna)))
         result = back_translate_gapped(protein, dna)
         self.compare_seqrecords(expected, result)
-
-    def test_missing_arg(self):
-        protein = SeqRecord(Seq("MK-V", alphabet=Gapped(IUPAC.protein)))
-        dna = SeqRecord(Seq("ATGAAAGTG", alphabet=IUPAC.unambiguous_dna))
-        self.assertRaises(MissingRecord, back_translate_gapped, protein, None)
-        self.assertRaises(MissingRecord, back_translate_gapped, None, dna)
-        self.assertRaises(MissingRecord, back_translate_gapped, None, None)
