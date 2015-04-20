887643c2240f6cc49c107a170d768f1eb4197b50
Kemal Eren <kemal@kemaleren.com>
2015-04-20 16:26:05 -0700

    added option to backtranslate.py to process sequences in order

--- backtranslate.py
+++ backtranslate.py
@@ -2,13 +2,14 @@
 """
 Back translate protein to DNA.
 
-Assumes sequences are in order in each file.
+Assumes sequences have the same id in each input file.
 
 Usage:
-  backtranslate.py <aligned_protein> <dna> <outfile>
+  backtranslate.py [options] <aligned_protein> <dna> <outfile>
   backtranslate.py -h | --help
 
 Options:
+  --in-order    Match sequences by order, instead of by name.
   -h --help     Show this screen.
 
 """
@@ -50,26 +51,30 @@ def back_translate_gapped(protein, dna):
     return result
 
 
-def backtranslate(protein_filename, dna_filename, outfile):
-    protein_dict = dict((s.id, s)
-                        for s in SeqIO.parse(protein_filename, "fasta",
-                                             alphabet=Gapped(IUPAC.protein)))
-    dna_dict = dict((s.id, s)
-                    for s in SeqIO.parse(dna_filename, "fasta",
-                                         alphabet=Gapped(IUPAC.unambiguous_dna)))
-    protein_ids = set(protein_dict)
-    dna_ids = set(dna_dict)
-    shared = protein_ids & dna_ids
-    missing_protein = dna_ids - protein_ids
-    missing_dna = protein_ids - dna_ids
-    if len(missing_protein) > 0:
-        sys.stderr.write('Warning: {} protein sequences'
-                         ' missing.'.format(len(missing_protein)))
-    if len(missing_dna) > 0:
-        raise Exception('{} protein sequences have no corresponding'
-                        ' dna sequence'.format(len(missing_dna)))
-    result_iter = (back_translate_gapped(protein_dict[_id], dna_dict[_id])
-                   for _id in shared)
+def backtranslate(protein_filename, dna_filename, outfile, inorder=False):
+    protein_records = SeqIO.parse(protein_filename, "fasta",
+                                  alphabet=Gapped(IUPAC.protein))
+    dna_records = SeqIO.parse(dna_filename, "fasta",
+                              alphabet=Gapped(IUPAC.unambiguous_dna))
+    if inorder:
+        result_iter = (back_translate_gapped(p, d)
+                       for p, d in zip(protein_records, dna_records))
+    else:
+        protein_dict = dict((s.id, s) for s in protein_records)
+        dna_dict = dict((s.id, s) for s in dna_records)
+        protein_ids = set(protein_dict)
+        dna_ids = set(dna_dict)
+        shared = protein_ids & dna_ids
+        missing_protein = dna_ids - protein_ids
+        missing_dna = protein_ids - dna_ids
+        if len(missing_protein) > 0:
+            sys.stderr.write('Warning: {} protein sequences'
+                             ' missing.'.format(len(missing_protein)))
+        if len(missing_dna) > 0:
+            raise Exception('{} protein sequences have no corresponding'
+                            ' dna sequence'.format(len(missing_dna)))
+        result_iter = (back_translate_gapped(protein_dict[_id], dna_dict[_id])
+                       for _id in shared)
     SeqIO.write(result_iter, outfile, "fasta")
 
 
@@ -78,4 +83,5 @@ if __name__ == "__main__":
     protein_filename = args["<aligned_protein>"]
     dna_filename = args["<dna>"]
     outfile = args["<outfile>"]
-    backtranslate(protein_filename, dna_filename, outfile)
+    inorder = args["--in-order"]
+    backtranslate(protein_filename, dna_filename, outfile, inorder=inorder)
