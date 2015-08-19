304b737a3a31dd4183eb828ba6d52265d63cbdd0
Kemal Eren <kemal@kemaleren.com>
2015-08-18 21:34:16 -0700

    wip: shift correction needs to take offsets into account

--- correct_shifts.py
+++ correct_shifts.py
@@ -19,7 +19,7 @@ Options:
 
 """
 
-from itertools import groupby
+from itertools import groupby, repeat
 from itertools import zip_longest
 from functools import partial
 import sys
@@ -44,7 +44,102 @@ def first_index(target, it):
     return -1
 
 
-def correct_shifts(seq, ref, gap_char=None, keep=False):
+def nth_nongap(seq, n, gap_char=None, reverse=False):
+    """
+
+    >>> nth_nongap('--a--bcd', 1)
+    2
+
+    >>> nth_nongap('--abc---', 1, reverse=True)
+    4
+
+    >>> nth_nongap('a--bcd', 1)
+    0
+
+    >>> nth_nongap('a--bcd', 2)
+    3
+
+    >>> nth_nongap('a--bc-d', 1, reverse=True)
+    6
+
+    >>> nth_nongap('a--bc-d', 2, reverse=True)
+    4
+
+    """
+    if gap_char is None:
+        gap_char = '-'
+    if reverse:
+        seq = list(reversed(seq))
+    count = 0
+    found = False
+    result = -1
+    for i, char in enumerate(seq):
+        if char != gap_char:
+            count += 1
+        if count == n:
+            found = True
+            result = i
+            break
+    if not found:
+        raise Exception('n is too large')
+    if reverse:
+        result = len(seq) - result - 1
+    return result
+
+
+def alignment_slice(ref, offsets=None, gap_char=None):
+    """
+    offsets are [start, stop]
+
+    >>> alignment_slice("aaccc---ggg", [1, 8])
+    (2, 11)
+
+    >>> alignment_slice("aaa--bbb", [0, 5])
+    (0, 8)
+
+    >>> alignment_slice("aaa--bb", [0, 4])
+    (0, 3)
+
+    >>> alignment_slice("aabbbcccd", [1, 9])
+    (2, 8)
+
+    >>> alignment_slice("abbbcccdd", [2, 10])
+    (1, 7)
+
+    >>> alignment_slice("abbbcccddd", [2, 11])
+    (1, 10)
+
+    >>> alignment_slice("---aaa---", [0, 2])
+    (0, 9)
+
+    """
+    # FIXME: leading and trailing insertions relative to reference
+    if gap_char is None:
+        gap_char = '-'
+    start = 0
+    stop = len(ref)
+    if offsets is None:
+        return start, stop
+    ungapped_len = sum(1 for c in ref if c != gap_char)
+    if offsets[1] - offsets[0] + 1 != ungapped_len:
+        raise Exception('offsets are wrong')
+    trim_start = (3 - (offsets[0] % 3)) % 3
+    start = nth_nongap(ref, trim_start + 1, gap_char)
+
+    trim_stop = (offsets[1] + 1) % 3
+    stop = nth_nongap(ref, trim_stop + 1, gap_char, reverse=True) + 1
+
+    new_len = len(list(c for c in ref[start:stop] if c != gap_char))
+    if (new_len % 3) != 0:
+        print(len(ref), ref)
+        print(offsets)
+        print(start, stop)
+        print(sum(1 for c in ref[start:stop] if c != gap_char))
+        raise Exception('alignment_slice failed to make an in-frame reference')
+    return start, stop
+
+
+def correct_shifts(seq, ref, offsets=None, gap_char=None, keep=False):
     """Correct frameshifts relative to a reference.
 
     keep: bool
@@ -55,7 +150,10 @@ def correct_shifts(seq, ref, gap_char=None, keep=False):
     # FIXME: make this codon-aware
     if gap_char is None:
         gap_char = '-'
-    if len(list(c for c in ref if c != gap_char)) % 3 != 0:
+    start, stop = alignment_slice(ref, offsets, gap_char)
+    seq = seq[start:stop]
+    ref = ref[start:stop]
+    if sum(1 for c in ref if c != gap_char) % 3 != 0:
         raise Exception('len(reference) is not a multiple of 3')
     result = []
     for k, g in groupby(zip_longest(seq, ref), key=partial(first_index, gap_char)):
@@ -91,7 +189,7 @@ def correct_shifts(seq, ref, gap_char=None, keep=False):
     return ''.join(result)
 
 
-def correct_shifts_fasta(infile, outfile, alphabet=None, keep=True):
+def correct_shifts_fasta(infile, outfile, offsetfile=None, alphabet=None, keep=True):
     """Correct all the pairs in a fasta file.
 
     Returns (n_seqs, n_fixed)
@@ -99,8 +197,13 @@ def correct_shifts_fasta(infile, outfile, alphabet=None, keep=True):
     """
     ldict = {}
     pairs = grouper(SeqIO.parse(infile, 'fasta', alphabet), 2)
-    results = (new_record_seq_str(seq, correct_shifts(seq.seq, ref.seq, keep=keep))
-               for seq, ref in pairs)
+    if offsetfile is None:
+        offsets = repeat(None)
+    else:
+        with open(offsetfile) as handle:
+            offsets = grouper(map(int, handle.read().split()), 2)
+    results = (new_record_seq_str(seq, correct_shifts(seq.seq, ref.seq, off, keep=keep))
+               for (seq, ref), off in zip(pairs, offsets))
 
     results = genlen(results, ldict, 'n_seqs')
     fixed = genlen(filter(lambda r: r.seq, results), ldict, 'n_fixed')
